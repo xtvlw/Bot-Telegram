a42875bb872777fdbdc738eac3d1b6d70b4e8cb5
_[N_F]_ <48897493+xtvlw@users.noreply.github.com>
2020-11-04 20:01:33 -0300

    Update unpacking.py

--- unpacking.py
+++ unpacking.py
@@ -1,5 +1,5 @@
 from os import system, listdir
-MangaName = "Koe-no-Katachi"
+MangaName = "Koe-no-katachi"
 
 class decode:
     def decoder(self, MangaName):
@@ -10,24 +10,24 @@ class decode:
             pt.ImgDetect(i, MangaName) #detect the image format to converter in PDF
 
     def mover(self, MangaName, capter):
-        system(f"mkdir -p /home/manjaro/Downloads/books/{MangaName} && \
-        mv /home/manjaro/Documentos/python/bot-telegram/content /home/manjaro/Downloads/books/{MangaName} && \
-        cd /home/manjaro/Downloads/books/{MangaName} && mv content capitulo-{capter}") #change the name of the folder to the respective capter
+        system(f"mkdir -p ./books/{MangaName}/{MangaName} && \
+        mv /home/manjaro/Documentos/python/bot-telegram/content ./books/{MangaName}/{MangaName} && \
+        cd ./books/{MangaName}/{MangaName} && mv content capitulo-{capter}") #change the name of the folder to the respective capter
 
 class PDFtransformer:
     def ImgDetect(self, i, MangaName):
-        type = listdir(f"/home/manjaro/Downloads/books/{MangaName}/capitulo-{i}") #get this local directory to verify the image format, to converter in a pdf file
+        type = listdir(f"./books/{MangaName}/{MangaName}/capitulo-{i}") #get this local directory to verify the image format, to converter in a pdf file
         typeImage = type[0][type[0].find('.'):] #get the final of the name of the archive, EX: teste.png => .png
-        pt.transformer(f"capitulo-{i}", f"/home/manjaro/Downloads/books/", f"{MangaName}", str(typeImage)) #send informations to converte the images in a pdf file
+        pt.transformer(f"capitulo-{i}", f"./books/{MangaName}/", MangaName, str(typeImage)) #send informations to converte the images in a pdf file
 
     def transformer(self, FolderName, diretory, MangaName, ImgFormat):
         system(f"convert {diretory}{MangaName}/{FolderName}/*{ImgFormat} {FolderName}-pdf.pdf") #convert all the images in a directory in a single pdf file
-        system(f"mkdir -p /home/manjaro/Downloads/books/{MangaName}PDF && \
-        mv /home/manjaro/Documentos/python/bot-telegram/{FolderName}-pdf.pdf /home/manjaro/Downloads/books/{MangaName}PDF/")#move all the PDFs to the correcdt directory
+        system(f"mkdir -p ./books/{MangaName}/{MangaName}PDF && \
+        mv /home/manjaro/Documentos/python/bot-telegram/{FolderName}-pdf.pdf ./books/{MangaName}/{MangaName}PDF/")#move all the PDFs to the correcdt directory
 
     def JoinPdf(self, MangaName):
-        system(f"pdftk /home/manjaro/Downloads/books/{MangaName}PDF/*.pdf cat output {MangaName}CompletePDF.pdf \
-        && mv /home/manjaro/Documentos/python/bot-telegram/{MangaName}CompletePDF.pdf /home/manjaro/Downloads/books/") #move the bigger PDF to the correct folder
+        system(f"pdftk ./books/{MangaName}/{MangaName}PDF/*.pdf cat output {MangaName}CompletePDF.pdf \
+        && mv /home/manjaro/Documentos/python/bot-telegram/{MangaName}CompletePDF.pdf ./books/{MangaName}/") #move the bigger PDF to the correct folder
 
 
 dc = decode()
